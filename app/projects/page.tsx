17588d9354a5eb22bb7a4e1831f51ac586169401
Jason Harris <jayyaj@hotmail.com>
2026-02-15 17:00:23 +0000

    Add captions to the lightbox

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 import Image from "next/image";
 import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
+import Captions from "yet-another-react-lightbox/plugins/captions";
+import "yet-another-react-lightbox/plugins/captions.css";
 
 export default function Projects() {
   const [index, setIndex] = useState(-1);
@@ -76,6 +78,7 @@ export default function Projects() {
         open={index >= 0}
         close={() => setIndex(-1)}
         slides={slides}
+        plugins={[Captions]}
         render={{
           slide: ({ slide, rect }) => {
             const width = Math.min(slide.width || 1200, rect.width);
