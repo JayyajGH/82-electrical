d5b31d27b13316e21e980018bf72424f5c3f01b1
Jason Harris <jayyaj@hotmail.com>
2026-02-16 20:35:57 +0000

    Tweak the wording

--- app/page.tsx
+++ app/page.tsx
@@ -7,20 +7,9 @@ export default function Home() {
       <section className="mb-12">
         <h2 className={`${styles.sectionHeading} heading1`}>About us</h2>
         <div className={styles.centeredText}>
-          <p className={styles.aboutText}>Offering domestic electrical services for Bristol and surrounding areas. From it's beginnings
-  in 2017 we have focused on delivering high quality, long lasting electrical installations both
-  on the surface and underneath. Designing, installing and fault finding most aspects of
-  electrical systems in the home. Always using products that are well tested to be long lasting
-  and efficient.</p>
-          <p className={styles.aboutText}>We try to keep things as simple as possible for our clients and always endeavour to be willing
-  to help in any way we can. Building systems that are future-proof and are designed around
-  our clients' long term needs as well as their short term ones. We take pride in providing safe
-  and reliable installations that are well suited for their intended purpose and are always happy
-  to help find solutions should any problems arise.</p>
-          <p className={styles.aboutText}>Hopefully this gives you some idea of our ethos as a company so please feel free to get in
-  touch via our "contact us" section if you have any specific queries and we will be happy to
-  answer them if we can.</p>
-          <p>Many thanks for stopping by and we look forward to hearing from you soon.</p>
+          <p className={styles.aboutText}>Offering domestic electrical services for Bristol and surrounding areas. Since our beginnings in 2017, we have focused on delivering high-quality, long-lasting electrical installations—both on the surface and underneath. We design, install, and fault-find most aspects of electrical systems in the home, always using products that are rigorously tested for efficiency and durability.</p>
+          <p className={styles.aboutText}>We aim to keep things as simple as possible for our clients and always endeavour to be helpful in any way we can. By building systems that are future-proof, we design around your long-term needs as well as your short-term ones. We take great pride in providing safe, reliable installations that are perfectly suited for their purpose, and we are always happy to find solutions should any problems arise.</p>
+          <p className={styles.aboutText}>Please feel free to get in touch via our "Contact Us" section with any specific queries, and we will be happy to answer them for you.</p>
         </div>
       </section>
       <section className="mb-12">
